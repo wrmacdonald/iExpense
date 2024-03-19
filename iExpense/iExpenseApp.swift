6e51834442be421e229bf9a8f25712110072f9ed
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-03-19 16:03:48 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Wes MacDonald on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
